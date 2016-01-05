0171c3bd0ad16f2e39616613dd1c0ecdd27fec25
Gary Guo <nbdd0121@hotmail.com>
2016-01-05 14:32:12 +0000

    Remove resolution specification to allow cache

--- Hooks.php
+++ Hooks.php
@@ -4,14 +4,13 @@ namespace Avatar;
 class Hooks {
 
 	public static function onGetPreferences($user, &$preferences) {
-		global $wgDefaultAvatarRes;
 		$link = \Linker::link(\SpecialPage::getTitleFor("AvatarUpload"), wfMsg('avatarupload'));
 
 		$preferences['editavatar'] = array(
 			'type' => 'info',
 			'raw' => true,
 			'label-message' => 'prefs-editavatar',
-			'default' => '<img src="' . \SpecialPage::getTitleFor("Avatar/{$user->getName()}/$wgDefaultAvatarRes")->getLinkURL() . '" width="32"></img> ' . $link,
+			'default' => '<img src="' . \SpecialPage::getTitleFor("Avatar/{$user->getName()}")->getLinkURL() . '" width="32"></img> ' . $link,
 			'section' => 'personal/info',
 		);
 
