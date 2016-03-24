b14810fa87ad6818a59779c7f9682b70e9e26631
Gary Guo <nbdd0121@hotmail.com>
2016-03-24 15:35:26 +0000

    zh-hant support

--- Avatar.alias.php
+++ Avatar.alias.php
@@ -10,3 +10,8 @@ $specialPageAliases['zh-hans'] = array(
 	'UploadAvatar' => array('上传头像'),
 	'ViewAvatar' => array('查看头像'),
 );
+
+$specialPageAliases['zh-hant'] = array(
+	'UploadAvatar' => array('上傳個人相片'),
+	'ViewAvatar' => array('檢視個人相片'),
+);
